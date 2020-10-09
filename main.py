c957d9bed125564966452ab3bd7f39b532b9df8d
Ulyses quart <ulyses@quart.com.mx>
2020-10-09 16:35:34 -0500

    ...

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.alert import Alert 
 from selenium import webdriver
 import chromedriver_autoinstaller
-from bs4 import BeautifulSoup
 import json
 import time
 import os
-from cassandra.cluster import Cluster
-from cassandra.auth import PlainTextAuthProvider
 import requests 
-from cassandra.query import SimpleStatement
 import sys
 import PyPDF2
+import uuid
+import cassandraSent as bd
+
+pathToHere=os.getcwd()
 
 options = webdriver.ChromeOptions()
 
@@ -70,9 +70,22 @@ if status==200:
     #find_elements_by_xpath will ALWAYS return a list
     print('Start reading the page...')
     for row in range(0,20):
+        pdfDownloaded=False
         for col in range(1,8):
             if col<7:
-                value=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==1:
+                    juris_rev=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==2:
+                    filetype=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==3:
+                    subject=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==4:
+                    fileNumber=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==5:
+                    summary=summary=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text
+                if col==6:
+                    date=browser.find_elements_by_xpath('//*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']')[0].text                    
+
             else:
                 #This is the xpath of the link : //*[@id="grdSentencias_ctl00__'+str(row)+'"]/td['+str(col)+']/a
                 #This find_element method works!
@@ -86,8 +99,26 @@ if status==200:
                     main_window=browser.window_handles[0]
                     pdf_window=browser.window_handles[1]
                     browser.switch_to_window(pdf_window)
+                    
+                    with open('json_sentencia.json') as json_file:
+                        json_sentencia = json.load(json_file)
+
+                    json_sentencia['id']=uuid.uuid4()
+                    json_sentencia['filenumber']=fileNumber
+                    json_sentencia['filetype']=filetype
+                    json_sentencia['jurisdictionalreviewer']=juris_rev
+                    # timestamp accepted for cassandra: yyyy-mm-dd 
+                    dateStr=date.split('/') #0:month,1:day,2:year
+                    dtDate=dateStr[2]+'-'+dateStr[0]+'-'+dateStr[1]
+                    json_sentencia['publication_datetime']=dtDate
+                    json_sentencia['strpublicationdatetime']=dtDate
+                    json_sentencia['subject']=subject
+                    json_sentencia['summary']=summary    
+
+
                     #Check if a pdf exists
                     for file in os.listdir(download_dir):
+                        pdfDownloaded=True
                         strFile=file.split('.')[1]
                         if strFile=='PDF' or strFile=='pdf':
                             pdfFileObj = open(download_dir+'\\'+file, 'rb')
@@ -100,14 +131,24 @@ if status==200:
                                 strFile=strFile+strContent
 
                             pdfFileObj.close()   
-                            
-                              
+
+                         
                     #When pdf is done and the record is in cassandra, delete all files in download folder
-                    for file in os.listdir(download_dir):
-                        os.remove(download_dir+'\\'+file)
+                    #If the pdf is not downloaded but the window is open, save the data without pdf
+                    if pdfDownloaded==True:
+                        json_sentencia['pdfcontent']=strFile
+                        for file in os.listdir(download_dir):
+                            os.remove(download_dir+'\\'+file) 
+                    else:
+                        json_sentencia['pdfcontent']=''
 
+                    #Insert information to cassandra
+                    res=bd.cassandraBDProcess(json_sentencia)
+                    if res:
+                        print('Sentencia added...')
+                    else:
+                        print('Something when wrong with cassandra')    
 
-                                
                     browser.close()
                     browser.switch_to_window(main_window)
                 else:
